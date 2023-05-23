229189b3a3cee580ec2bf2cfbf169241f598a181
Áthila Naniny Ninow <athilann@gmail.com>
2023-05-23 21:09:59 +0100

    primeira execucao

--- sessoes/sessao4/redes_neurais_artificiais/breast_cancer_simples.py
+++ sessoes/sessao4/redes_neurais_artificiais/breast_cancer_simples.py
@@ -8,4 +8,13 @@ classe = pd.read_csv('saidas_breast.csv')
 from sklearn.model_selection import train_test_split
 previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe,test_size=0.25)
 
-print("done")
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+classificador = Sequential()
+classificador.add(Dense(units = 16, activation = 'relu', kernel_initializer = 'random_uniform', input_dim = 30))
+#classificador.add(Dense(units = 16, activation = 'relu', kernel_initializer = 'random_uniform'))
+classificador.add(Dense(units = 1, activation = 'sigmoid'))
+otimizador = keras.optimizers.Adam(lr = 0.001, decay = 0.0001, clipvalue = 0.5)
+classificador.compile(optimizer = otimizador, loss = 'binary_crossentropy', metrics = ['binary_accuracy'])
+classificador.fit(previsores_treinamento, classe_treinamento, batch_size = 10, epochs = 100)
